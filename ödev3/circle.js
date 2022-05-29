084696233ea94be28662ab0726eceffb242c1434
yufus7 <79915956+yufus7@users.noreply.github.com>
2022-05-29 15:35:33 +0300

    ödev 3

---
+++ ödev3/circle.js
@@ -0,0 +1,13 @@
+function circleArea(r) {
+    console.log(Math.PI * (r * r));
+
+}
+
+function circleCircumference(r) {
+    console.log(2 * Math.PI * r);
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference,
+};
---
+++ ödev3/index.js
@@ -0,0 +1,5 @@
+const circle = require('./circle.js');
+
+
+circle.circleArea(5)
+circle.circleCircumference(5)
